4613180bd5f1c5a5e8065a1f4f4a83a2ea314303
Isidora <isidora.oyanedel@usach.cl>
2024-03-29 17:06:32 -0300

    avance

---
+++ backend_tingeso/src/main/java/com/example/backend_tingeso/entities/CarEntity.java
@@ -0,0 +1,27 @@
+package com.example.backend_tingeso.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity //va asi tal cual
+@Table(name = "car")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CarEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+
+    private String patent; //patente de letras y numeros
+    private String brand; //marca del vehiculo
+    private String model; //modelo del vehiculo suzuki swift -> swift modelo
+    private int type; //tipo de vehiculo
+    private int productionYear; //año de fabricacion
+    private String motorType; //tipo de motor gasolina, diesel, hibrido y electrico
+    private int numberSeats; //numero de asientos
+}
